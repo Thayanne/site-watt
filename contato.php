04f4608dc25bd297fb6406bd85cfe80da332926e
Lucas de Sousa <lucasnadalutti@gmail.com>
2013-04-04 03:07:28 -0300

    Teste PHP 3

--- contato.php
+++ contato.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<?
+<?php
 
 $nome=$_POST['nome'];
 $email=$_POST['email'];
@@ -27,13 +20,3 @@ mail("$Destinatario", "$mensagem","From:$email");
 echo "<script>alert (\"Sua mensagem foi enviada com sucesso para o nosso e-mail. Obrigado!\")</script>";
 
 ?>
-<html>
-<body>
-Enviado com sucesso !<br />
-<a href="index.html">Voltar!</a>
-</body>
-</html>
-
-<body>
-</body>
-</html>
